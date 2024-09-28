269c74de1749dfd2baae27b3f80b8e3528643eaf
Jamerzbatman <jamiethetec@gmail.com>
2024-09-28 08:04:51 -0700

    it works

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def dashboard_view(request):
-    return render(request, 'pages/dashbaordpage.html')
+    return render(request, 'dashboard/dashbaordpage.html')
